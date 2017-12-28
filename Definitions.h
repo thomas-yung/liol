76d2894585a43c4731ab1e6d37aeb43f35dbe7ee
Thomas Yung <thomasyung@hotmail.co.uk>
2017-12-28 12:39:49 +0000

    [ty] Complete and manually tested command-line game

--- Definitions.h
+++ Definitions.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 // Macros
 #define RANDOMNESS_CAP 7
--- Main.c
+++ Main.c
@@ -30,6 +30,8 @@ int main(int ARGC, char** ARGV) {
   // Free resources
   freeResources();
 
+  printf("\nQuitting...\n\n");
+
   exit(EXIT_SUCCESS);
 }
 
@@ -42,46 +44,52 @@ void gatherPlayerData() {
   srand(time(NULL));
 
   // Poll user for number of players
-  numberOfPlayers = 6;
-  printf("Please enter the number of players (one or more):\n");
-  // scanf("%d", &numberOfPlayers);
+  numberOfPlayers = 0;
+  while (numberOfPlayers <= 0) {
+    printf("Please enter the number of players (one or more):\n");
+    scanf("%d", &numberOfPlayers);
+  }
+
   state->numberOfPlayers = numberOfPlayers;
   state->names = calloc(numberOfPlayers, sizeof(char*));
   state->scores = calloc(numberOfPlayers, sizeof(int));
 
   // Poll user for player names
-  // for (int i = 0; i < numberOfPlayers; i++) {
-  //   printf("Please enter player %d's name:\n", i);
-  //   char nameBuffer[256];
-  //   scanf("%s", nameBuffer);
-  //   (state->names)[i] = nameBuffer;
-  //   (state->scores)[i] = 0;
-  // }
+  for (int i = 0; i < numberOfPlayers; i++) {
+    (state->names)[i] = calloc(256, sizeof(char));
+
+    printf("Please enter player %d's name:\n", i);
+    scanf("%s", (state->names)[i]);
+
+    (state->scores)[i] = 0;
+  }
 
   // Randomise play order
   state->playOrder = calculatePlayOrder();
 }
 
 // Ramdomly arrange the numbers from 0 to numberOfPlayers
+// Add players to resulting order by stepping over unoccupied (-1) spaces
+// a random number of times
 int *calculatePlayOrder() {
-  // Add players to result by randomly hopping through the list, changing a
-  // used number to -1 to show it has been selected
   int *result = calloc(numberOfPlayers, sizeof(int));
+
+  // Initialise all entries to -1
   for (int i = 0; i < numberOfPlayers; i++) {
     result[i] = -1;
   }
 
   int currentIndex = 0;
   for (int i = 0; i < numberOfPlayers - 1; i++) {
-    int hops = rand() % RANDOMNESS_CAP;
 
+    int hops = rand() % RANDOMNESS_CAP;
     while (hops >= 0) {
       currentIndex = (currentIndex + 1) % numberOfPlayers;
+
       if (result[currentIndex] == -1) {
         // There is not a number in place so decrement hops
         hops--;
       }
-
     }
 
     result[currentIndex] = i;
@@ -99,12 +107,12 @@ int *calculatePlayOrder() {
 
 // Run the game
 void runGameLoop() {
-
   printf("The play order is:\n");
 
   for (int i = 0; i < numberOfPlayers; i++) {
-    printf("(%d) %s\n", i, (state->names)[(state->playOrder)[i]]);
+    printf("\t(%d) %s\n", i, (state->names)[(state->playOrder)[i]]);
   }
+
   printf("\nLet's begin!\n\n");
 
   int numberOfAlivePlayers = numberOfPlayers;
@@ -112,14 +120,14 @@ void runGameLoop() {
   int roundNumber = 1;
 
   while (numberOfAlivePlayers) {
-    printf("------ Round %d ------\n", roundNumber);
+    printf("------------------------ Round %d ------------------------\n", roundNumber);
 
     for (int i = 0; i < numberOfPlayers; i++) {
       int currentPlayerIndex = (state->playOrder)[currentPlayer];
 
       if (currentPlayerIndex > -1) {
         // Player is alive
-        printf("%s, it's your turn to go!\n",
+        printf("\n%s, it's your turn to go!\n",
                 (state->names)[currentPlayerIndex]);
 
         // Spin for current player
@@ -127,39 +135,47 @@ void runGameLoop() {
         int score = printInstruction(target);
 
         // Await success/fail response
-        printf("Please enter 'y' for success and 'n' for failure:\n");
-        char res[1];
-        scanf("%c", res);
+        char res;
+        do {
+          printf("Please enter 'y' for success and 'n' for failure:\n> ");
+          scanf(" %c", &res);
+        } while (res != 'y' && res != 'n');
 
         // Update scores
-        if (*res == 'y') {
+        if (res == 'y') {
           // Successful move
           (state->scores)[currentPlayerIndex] += score;
         } else {
           // Unsuccessful move
           (state->playOrder)[currentPlayer] = -1;
+          numberOfAlivePlayers--;
         }
+
       } else {
         // Player is dead
-        // Do nothing
+          // Do nothing
       }
+
+      currentPlayer = (currentPlayer + 1) % numberOfPlayers;
     }
 
-    printf("Thats the end of round %d, here are the scores...", roundNumber);
+    printf("\nThats the end of round %d, here are the scores...\n", roundNumber);
     for (int i = 0; i < numberOfPlayers; i++) {
       char *name = (state->names)[i];
       int score = (state->scores)[i];
       printf("\t%s has %d points\n", name, score);
     }
 
+    printf("---------------------------------------------------------\n\n");
 
-
+    roundNumber++;
   }
 
+  printf("\nGame over! Let's have a look at the scores...\n");
   displayResults();
 }
 
-// TODO: explain
+// Display the results *at the end of the game* i.e show winner(s) too
 void displayResults() {
   char *winnerName = NULL;
   int winnerScore = 0;
@@ -176,11 +192,11 @@ void displayResults() {
       gameTied = 1;
     }
 
-    printf("%s scored %d points\n", name, score);
+    printf("\t%s scored %d points\n", name, score);
   }
 
   if (gameTied) {
-    printf("There are no winners today, you're all losers!\n");
+    printf("\nThere are no winners today, you're all losers!\n");
     printf("However these people lost less than the others...\n");
 
     for (int i = 0; i < numberOfPlayers; i++) {
@@ -197,6 +213,7 @@ void displayResults() {
 
 // TODO: abstract
 int printInstruction(int target) {
+
   switch (target) {
     case 0:
       printf("Get his FACE\n");
@@ -233,8 +250,8 @@ void freeResources() {
   for (int i = 0; i < numberOfPlayers; i++) {
     free((state->names)[i]);
   }
-  free(state->names);
 
+  free(state->names);
   free(state->scores);
   free(state->playOrder);
 
